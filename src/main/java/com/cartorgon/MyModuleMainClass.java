29e90107d8bbc75a286d9a26d93b66576f041fc2
Carlos Torres <carlos.a.torres-gonzalez@capgemini.com>
2019-08-30 09:43:27 +0200

    Initial commit master branch

---
+++ src/main/java/com/cartorgon/MyModuleMainClass.java
@@ -0,0 +1,7 @@
+package com.cartorgon;
+
+public class MyModuleMainClass {
+	public static final void main(final String[] args) {
+		System.out.println("I'm in 'MyModuleMainClass.main'");
+	}
+}
---
+++ src/main/java/com/cartorgon/module-info.java
@@ -0,0 +1,7 @@
+module com.cartorgon {
+	/*
+	 * Not really required since all the modules are going to need base types.
+	 * It's included/required just for demo purposes
+	 */
+	requires java.base;	
+}
